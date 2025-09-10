51f2e7ea40718a67cf56489bf8dafe39a8013cdf
Sarvin Ravinthiran <sarvin.ravinthiran@ontariotechu.net>
2025-09-10 10:47:43 -0400

    Lab 01 completed code

--- lab01.py
+++ lab01.py
@@ -23,7 +23,7 @@ def main():
     investment += investment * interest_rate
     investment += investment * interest_rate
     investment += investment * interest_rate
-    print('After 5 years, your investment will be worth ' + str(investment) + ' dollars.')
+    print('After 5 years, your investment will be worth ' + str(investment) + ' dollars.') #turned float into string
     # expected output: After 5 years, your investment will be worth 1187.6863056468749 dollars.
 
 if __name__ == "__main__":
